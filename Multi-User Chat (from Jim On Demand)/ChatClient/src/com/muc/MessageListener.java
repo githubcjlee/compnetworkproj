19129391e56c4fb0bbfc92d635f0c737e9b3ea90
ted <ted@cometnet-10-21-13-118.utdallas.edu>
2018-04-21 13:33:17 -0500

    change

--- Multi-User Chat (from Jim On Demand)/ChatClient/src/com/muc/MessageListener.java
+++
@@ -1,8 +0,0 @@
-package com.muc;
-
-/**
- * Created by jim on 4/21/17.
- */
-public interface MessageListener {
-    public void onMessage(String fromLogin, String msgBody);
-}
--- Multi-User Chat (from Jim On Demand)/ChatClient/src/com/muc/UserStatusListener.java
+++
@@ -1,9 +0,0 @@
-package com.muc;
-
-/**
- * Created by jim on 4/21/17.
- */
-public interface UserStatusListener {
-    public void online(String login);
-    public void offline(String login);
-}
